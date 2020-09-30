1915e97df95e19a7d9f96fbb71cf5315d4a38683
Sandeep <Sandeepkondreddy@gmail.com>
2020-09-30 12:52:24 +0530

    Firt Commit15

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -1,13 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/cordova-plugin-device/www/device.js",
-        "id": "cordova-plugin-device.device",
-        "clobbers": [
-            "device"
-        ]
-    },
-    {
         "file": "plugins/com.joandilee.imeiplugin/www/imeiplugin.js",
         "id": "com.joandilee.imeiplugin.imeiplugin",
         "clobbers": [
@@ -19,7 +12,6 @@ module.exports.metadata =
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.2.2",
-    "cordova-plugin-device": "2.0.3",
     "com.joandilee.imeiplugin": "0.1"
 };
 // BOTTOM OF METADATA
